b42da3605c0c26e271ba020dbfd4fea3a885df55
zhangpeng <scbeta@qq.com>
2020-05-20 17:40:59 +0800

    初步完成移植。

---
+++ Xceed.Wpf.Toolkit/CollectionControl/Implementation/Converters/TypeNameConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Reflection;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace Xceed.Wpf.Toolkit.Converters
+{
+    public class TypeNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var type = value as Type;
+            if (type == null)
+                return null;
+            var typeName = string.Join(" - ", type.GetCustomAttributes(typeof(DisplayNameAttribute), true)
+                .Select(t => (DisplayNameAttribute)t)
+                .Select(t => t.DisplayName));
+            if (string.IsNullOrEmpty(typeName))
+                typeName = type.Name;
+            return typeName;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
